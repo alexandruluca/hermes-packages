e9d07d5a5e212b8d94b760b81dd02460b4cd9e61
Luca Alexandru <luca.p.alexandru@gmail.com>
2019-12-23 17:06:38 +0200

    debug

--- hermes-packages-server/src/app.js
+++ hermes-packages-server/src/app.js
@@ -92,11 +92,13 @@ swaggerTools.initializeMiddleware(require('./api/specs.json'), function (middlew
 	});
 
 	app.get('/api/authorize', async(req, res, next) => {
+		console.log('auth');
 		const AUTHORIZE_URL = "https://github.com/login/oauth/authorize";
-		const REDIRECT_URI = "http://localhost:8090/api/callback";
+		const REDIRECT_URI = "https://172.16.0.30/api/callback";
 		const ENCODED_REDIRECT_URI = encodeURIComponent(REDIRECT_URI);
 		const redirectUrl = `${AUTHORIZE_URL}?scope=repo&client_id=d078dfb8b4eb0175d59f&redirect_uri=${ENCODED_REDIRECT_URI}`;
 
+		console.log(redirectUrl);
 		res.redirect(redirectUrl);
 	});
 
@@ -112,7 +114,7 @@ swaggerTools.initializeMiddleware(require('./api/specs.json'), function (middlew
 						client_id: 'd078dfb8b4eb0175d59f',
 						client_secret: 'f5da3cebb6532d2e7f562acaa2df8b080b228216',
 						code: code,
-						redirect_uri: 'http://localhost:8090/api/callback'
+						redirect_uri: 'https://172.16.0.30/api/callback'
 					}
 				}
 			);
